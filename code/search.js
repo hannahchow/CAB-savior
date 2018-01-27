e228e6b51fd4a986684c551fec14ce0b06325358
Hannah Chow <hannah_chow@brown.edu>
2018-01-27 17:24:34 -0500

    weird stuff is happening

--- code/search.js
+++ code/search.js
@@ -7,8 +7,13 @@ window.onload = function() {
     .then(res => res.json())
     .then((out) => {
       console.log('Checkout this JSON! ', out);
+      document.getElementById("coursecode").innerHTML = out.code;
+      document.getElementById("classname").innerHTML = out.title;
+      document.getElementById("classsize").innerHTML = out.sections[0].capacity;
+      document.getElementById("classavail").innerHTML = out.sections[0].avail;
     })
     .catch(err => { throw err });
     return false;
   }
+
 }
