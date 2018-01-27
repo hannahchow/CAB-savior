2b8c12534935e98106eeb5da6a6761b5e9da4839
amja <me@amosjackson.com>
2018-01-27 14:51:29 -0500

    Some comments and cleaning up

--- node_server/server.js
+++ node_server/server.js
@@ -1,6 +1,10 @@
-send_email => {
-	var email = require("emailjs");
+var http = require('http');
+var https = require('https');
+const { URL, URLSearchParams } = require('url');
+var email = require("emailjs");
 
+// TODO: Fix this send email function.
+send_email => {
 	var server = email.server.connect({
 	   user:    "cabsavior@gmail.com", 
 	   password:"Graduate20", 
@@ -18,23 +22,24 @@ send_email => {
 	}, function(err, message) { console.log(err || message); });
 }
 
-var http = require('http');
-var https = require('https');
-const { URL, URLSearchParams } = require('url');
-
+// Sets up the node server.
 var server = http.createServer(function(req, res) {
+	// Gets parameters from url and checks if there is one for course code.
 	var code = "";
 	var params = new URLSearchParams(req.url.substr(2));
+	// Iterates to search for course_code.
 	for (const [name, value] of params) {
 	  if(name == "course_code") {
 	  	code = value;
 	  }
 	}
-	var body = "";
+
+	// Error if no course code.
 	if(code == "") {
 		res.writeHead(400);
 		res.end("Error: course code not supplied!");
 	} else {
+		// Gets data from that course code!
 		https.get('https://cab.brown.edu/asoc-api/?output=json&page=asoc.rjs&route=course&term=201720&course=' + code, (resp) => {
 			let data = '';
 
@@ -43,10 +48,13 @@ var server = http.createServer(function(req, res) {
 			data += chunk;
 			});
 
-			// The whole response has been received. Print out the result.
+			// The whole response has been received.
 			resp.on('end', () => {
-				var parsed = JSON.parse(data);
+				// Success.
 				res.writeHead(200);
+
+				var parsed = JSON.parse(data);
+				// Checks if API returned error.
 				if(parsed['error'] != null) {
 					res.end("Error: course not found!");
 				} else {
