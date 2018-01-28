03f9e34bf9dbb98aded191aeef86790018f6481d
Pete <kelly.peterhale@gmail.com>
2018-01-27 22:25:18 -0500

    just some background stuff

---
+++ code/background.js
@@ -0,0 +1,12 @@
+var i = 1;                     //  set your counter to 1
+
+function myLoop () {           //  create a loop function
+   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+      console.log("Hello");          //  your code here                  //  increment the counter
+      if (i < 10) {            //  if the counter < 10, call the loop function
+         myLoop();             //  ..  again which will trigger another 
+      }                        //  ..  setTimeout()
+   }, 3000)
+}
+
+myLoop();
