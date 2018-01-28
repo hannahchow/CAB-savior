1f07773dd0f82765a95a3ba74ba2d9a7804c9502
amja <me@amosjackson.com>
2018-01-27 19:45:29 -0500

    New function and some more set code

--- code/search.js
+++ code/search.js
@@ -1,25 +1,29 @@
+function populate(out) {
+  document.getElementById("coursecode").innerHTML = out.code;
+  document.getElementById("classname").innerHTML = out.title;
+  document.getElementById("classsection").innerHTML = out.sections[0].no;
+  document.getElementById("classsize").innerHTML = out.sections[0].capacity;
+  document.getElementById("classavail").innerHTML = out.sections[0].avail;
+  document.getElementById("container").style.display = "block";
+}
+
 window.onload = function() {
 
   var ele = document.getElementById('course-finder');
+  // chrome.storage.sync.get('classPicked', function(items) {
+  //   console.log(items);
+  // });
   ele.onsubmit = function() {
     var url = "http://10.38.58.25:8080/?course_code=" + document.getElementById('search').value;
     fetch(url)
     .then(res => res.json())
     .then((out) => {
-      // chrome.storage.sync.set({'value': "hello"}, function() {
+      // chrome.storage.sync.clear();
+      // console.log('Checkout this JSON! ', out);
+      populate(out);
+      // chrome.storage.sync.set({'classPicked': out}, function() {
       //   console.log("we saved");
       // });
-      // chrome.storage.sync.clear();
-      // chrome.storage.sync.get('value', function(items) {
-      //   console.log(items);
-      // });
-      // console.log('Checkout this JSON! ', out);
-      document.getElementById("coursecode").innerHTML = out.code;
-      document.getElementById("classname").innerHTML = out.title;
-      document.getElementById("classsection").innerHTML = out.sections[0].no;
-      document.getElementById("classsize").innerHTML = out.sections[0].capacity;
-      document.getElementById("classavail").innerHTML = out.sections[0].avail;
-      document.getElementById("container").style.display = "block";
     })
     .catch(err => { throw err });
     return false;
