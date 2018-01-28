760c42ebfb722dcf0f36c1abf36a22e56fbd9d24
Hannah Chow <hannah_chow@brown.edu>
2018-01-27 20:13:10 -0500

    if / else cases search

--- code/search.js
+++ code/search.js
@@ -2,30 +2,36 @@ function populate(out) {
   document.getElementById("coursecode").innerHTML = out.code;
   document.getElementById("classname").innerHTML = out.title;
   document.getElementById("classsection").innerHTML = out.sections[0].no;
-  document.getElementById("classsize").innerHTML = out.sections[0].capacity;
-  document.getElementById("classavail").innerHTML = out.sections[0].avail;
+
+  
+  if (out.sections[0].capacity == "") {
+    document.getElementById("classsize").innerHTML = "uncapped";
+    document.getElementById("classavail").innerHTML = "uncapped";
+  } else {
+    document.getElementById("classsize").innerHTML = out.sections[0].capacity;
+    document.getElementById("classavail").innerHTML = out.sections[0].avail;
+  }
+
   document.getElementById("container").style.display = "block";
 }
 
 window.onload = function() {
 
   var ele = document.getElementById('course-finder');
-  chrome.storage.sync.get('classPicked', function(items) {
-    if(items['classPicked'] != null) {
-      populate(items['classPicked']);
-    }
-  });
+  // chrome.storage.sync.get('classPicked', function(items) {
+  //   console.log(items);
+  // });
   ele.onsubmit = function() {
     var url = "http://10.38.58.25:8080/?course_code=" + document.getElementById('search').value;
     fetch(url)
     .then(res => res.json())
     .then((out) => {
+      // chrome.storage.sync.clear();
       // console.log('Checkout this JSON! ', out);
       populate(out);
-      chrome.storage.sync.set({'classPicked': out}, function() {
-        console.log("we saved");
-      });
-      
+      // chrome.storage.sync.set({'classPicked': out}, function() {
+      //   console.log("we saved");
+      // });
     })
     .catch(err => { throw err });
     return false;
