9cd83081fea9cb8285363aa2b17d50971905904b
amja <me@amosjackson.com>
2018-01-27 22:08:04 -0500

    Clean and commented

--- code/search.js
+++ code/search.js
@@ -1,9 +1,13 @@
+/**
+ * This function propagates the HTML with appropriate information depending on the user's search.
+ */
 function populate(out) {
 
   var warning = document.getElementById("error");
 
   if (out.error != null) {
 
+    // Shows error and hides everything else.
     warning.style.display = "block";
     warning.children[1].innerHTML = out.error;
     Array.from(document.getElementsByClassName("separated")).forEach(function(item) {
@@ -11,24 +15,29 @@ function populate(out) {
     });
     document.getElementById('register').style.display = "none";
   } else {
+    // Hides error and shows everything else.
+    warning.style.display = "none";
     Array.from(document.getElementsByClassName("separated")).forEach(function(item) {
       item.style.display = "block";
     });
     document.getElementById('register').style.display = "block";
-    warning.style.display = "none";
 
+  // Clear sections so repeated requests don't add on to the old list.
   document.getElementById('sections').innerHTML = "";
-  document.getElementById("coursecode").innerHTML = out.code;
 
+  // Inserts some data.
+  document.getElementById("coursecode").innerHTML = out.code;
+  document.getElementById("classname").innerHTML = out.title;
+
+  // Shows override warning if appropriate.
   var override = document.getElementById("courseoverride");
   if (out.overrideRequired == true){
     override.style.display = "block";
   } else {
     override.style.display = "none";
   }
-  document.getElementById("classname").innerHTML = out.title;
 
-
+  // Makes an HTML block for each section, giving each its appropriate information.
   out.sections.forEach(function(section){
     var classsize = 0;
     var classavail = 0;
@@ -59,19 +68,21 @@ function populate(out) {
       document.getElementById('sections').innerHTML = document.getElementById('sections').innerHTML + html;
 
   });
-  // document.getElementById('classcontainer').innerHTML = document.getElementById('classcontainer').innerHTML + '<a href="https://cab.brown.edu/" target="_blank" id="register">REGISTER FOR COURSE</a>';
+  // Show the hidden block (initially, only the search bar is visible).
   document.getElementById("container").style.display = "block";
  }
 }
 
 window.onload = function() {
-
-  var ele = document.getElementById('course-finder');
+  // If data previously fetched, propagates from local storage.
   chrome.storage.sync.get('classPicked', function(items) {
      if(items['classPicked'] != null) {
        populate(items['classPicked']);
      }
    });
+
+  // Gets data from user's search
+  var ele = document.getElementById('course-finder');
   ele.onsubmit = function() {
     var url = "http://10.38.58.25:8080/?course_code=" + document.getElementById('search').value;
     fetch(url)
