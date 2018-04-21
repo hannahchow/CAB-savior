6a2cf521aed0386080c4ad5cb5f29f096b896602
amja <me@amosjackson.com>
2018-04-21 19:57:24 -0400

    Catching errors etc.

--- code/background.js
+++ code/background.js
@@ -6,7 +6,6 @@ function myLoop () {
 	window.setTimeout(function() {
 	   	 chrome.storage.sync.get('classPicked', function(items) {
 	   	  if(items['classPicked'] != null) {
-	   	  	console.log("Looping");
 	   	  	var url = "https://cab-savior.herokuapp.com/?course_code=" + items['classPicked'].code;
 	   	   fetch(url)
 	   	     .then(res => res.json())
@@ -15,19 +14,22 @@ function myLoop () {
 	   	       avail_num = class_data.sections[0].avail;
 	   	       if (old <= 0 && avail_num > 0) {
 	   	         chrome.notifications.create(null, {type: "basic",title: "Spaces available!",message: "A space has opened up in " + class_data.title, iconUrl: "icon.png"});
-	   	         chrome.storage.sync.get('email', function(items) {
-	   	       if(items['email'] != null) {
-	   	         var url = "https://cab-savior.herokuapp.com/?email=" + items['email'] + "&course_code=" + items['classPicked'].code;
-	   	         fetch(url);
-	   	       }
+	   	         chrome.storage.sync.get(['email', 'classPicked'], function(items) {
+		   	       if(items['email'] != null) {
+		   	         var url = "https://cab-savior.herokuapp.com/?email=" + items['email'] + "&course_code=" + items['classPicked'].code;
+		   	         fetch(url);
+		   	       }
 	   	         });
 	   	       }
 	   	       old = avail_num;
 	   	     })
+	   	     .catch(function(error) {
+	   	     	console.log(error);
+	   	     })
 	   	   myLoop();
 			} 
 		});
-	},3000);
+	},1000 * 3);
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
